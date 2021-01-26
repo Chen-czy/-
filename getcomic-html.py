2ebd15769505fde3fb7f4dbecad08f61e075247c
Chen-czy <62040509+Chen-czy@users.noreply.github.com>
2021-01-26 16:10:25 +0800

    Create getcomic-html.py

---
+++ getcomic-html.py
@@ -0,0 +1,22 @@
+import requests
+import os
+from bs4 import BeautifulSoup
+
+lastChapterId = 0
+nowChapterId = 0
+chapterCnt = 0
+mhName = ""
+try:
+    os.makedirs("./result/" + mhName)
+except FileExistsError:
+    pass
+while nowChapterId <= lastChapterId:
+    chapterCnt += 1
+    comicPage = requests.get("https://www.kanmeizi.cc/chapter/" + str(nowChapterId))
+    soup1 = BeautifulSoup(comicPage.text, "lxml")
+    imglist = soup1.select(".lazy")
+    for i in range(1, len(imglist) + 1):
+        soup2 = BeautifulSoup(str(imglist[i - 1]), "lxml")
+        with open("./result/" + mhName + "/第" + str(chapterCnt) + "话.html", "a") as docWriter:
+            docWriter.write("<img src=\"" + soup2.img["data-original"] + "\">")
+    nowChapterId += 1
